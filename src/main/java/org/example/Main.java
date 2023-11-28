47dc1544848a69db39643c54882784e85a0d9de8
Andrey <andreiperevertov@yandex.ru>
2023-11-28 16:25:12 +0300

    Test 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello worl22222d!33333333333");
+        System.out.println("Hello worl22222d!33333333333_branch_main2");
     }
 }
